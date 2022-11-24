6622ce950f1399333a4d9f3e7fb5f69407e97b22
ankitaS11 <ankitalrm@gmail.com>
2022-11-24 20:43:50 +0530

    reading images from folder in darkprior.py

--- Image Processing/Code/darkprior.py
+++ Image Processing/Code/darkprior.py
@@ -4,7 +4,7 @@ import numpy as np;
 
 def DarkChannel(im,sz):
     b,g,r = cv2.split(im)
-    dc = cv2.min(cv2.min(r,g),b);
+    dc = cv2.min(cv2.min(r,g),b)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(sz,sz))
     dark = cv2.erode(dc,kernel)
     return dark
@@ -13,59 +13,59 @@ def AtmLight(im,dark):
     [h,w] = im.shape[:2]
     imsz = h*w
     numpx = int(max(math.floor(imsz/1000),1))
-    darkvec = dark.reshape(imsz);
-    imvec = im.reshape(imsz,3);
+    darkvec = dark.reshape(imsz)
+    imvec = im.reshape(imsz,3)
 
-    indices = darkvec.argsort();
+    indices = darkvec.argsort()
     indices = indices[imsz-numpx::]
 
     atmsum = np.zeros([1,3])
     for ind in range(1,numpx):
        atmsum = atmsum + imvec[indices[ind]]
 
-    A = atmsum / numpx;
+    A = atmsum / numpx
     return A
 
 def TransmissionEstimate(im,A,sz):
-    omega = 0.95;
-    im3 = np.empty(im.shape,im.dtype);
+    omega = 0.95
+    im3 = np.empty(im.shape,im.dtype)
 
     for ind in range(0,3):
         im3[:,:,ind] = im[:,:,ind]/A[0,ind]
 
-    transmission = 1 - omega*DarkChannel(im3,sz);
+    transmission = 1 - omega*DarkChannel(im3,sz)
     return transmission
 
 def Guidedfilter(im,p,r,eps):
-    mean_I = cv2.boxFilter(im,cv2.CV_64F,(r,r));
-    mean_p = cv2.boxFilter(p, cv2.CV_64F,(r,r));
-    mean_Ip = cv2.boxFilter(im*p,cv2.CV_64F,(r,r));
-    cov_Ip = mean_Ip - mean_I*mean_p;
+    mean_I = cv2.boxFilter(im,cv2.CV_64F,(r,r))
+    mean_p = cv2.boxFilter(p, cv2.CV_64F,(r,r))
+    mean_Ip = cv2.boxFilter(im*p,cv2.CV_64F,(r,r))
+    cov_Ip = mean_Ip - mean_I*mean_p
 
-    mean_II = cv2.boxFilter(im*im,cv2.CV_64F,(r,r));
-    var_I   = mean_II - mean_I*mean_I;
+    mean_II = cv2.boxFilter(im*im,cv2.CV_64F,(r,r))
+    var_I   = mean_II - mean_I*mean_I
 
-    a = cov_Ip/(var_I + eps);
-    b = mean_p - a*mean_I;
+    a = cov_Ip/(var_I + eps)
+    b = mean_p - a*mean_I
 
-    mean_a = cv2.boxFilter(a,cv2.CV_64F,(r,r));
-    mean_b = cv2.boxFilter(b,cv2.CV_64F,(r,r));
+    mean_a = cv2.boxFilter(a,cv2.CV_64F,(r,r))
+    mean_b = cv2.boxFilter(b,cv2.CV_64F,(r,r))
 
-    q = mean_a*im + mean_b;
-    return q;
+    q = mean_a*im + mean_b
+    return q
 
 def TransmissionRefine(im,et):
-    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY);
-    gray = np.float64(gray)/255;
-    r = 60;
-    eps = 0.0001;
-    t = Guidedfilter(gray,et,r,eps);
+    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    gray = np.float64(gray)/255
+    r = 60
+    eps = 0.0001
+    t = Guidedfilter(gray,et,r,eps)
 
-    return t;
+    return t
 
 def Recover(im,t,A,tx = 0.1):
-    res = np.empty(im.shape,im.dtype);
-    t = cv2.max(t,tx);
+    res = np.empty(im.shape,im.dtype)
+    t = cv2.max(t,tx)
 
     for ind in range(0,3):
         res[:,:,ind] = (im[:,:,ind]-A[0,ind])/t + A[0,ind]
@@ -74,28 +74,26 @@ def Recover(im,t,A,tx = 0.1):
 
 if __name__ == '__main__':
     import sys
-    try:
-        fn = sys.argv[1]
-    except:
-        fn = 'output.jpeg'
-
-    def nothing(*argv):
-        pass
-
-    src = cv2.imread(fn);
-
-    I = src.astype('float64')/255;
- 
-    dark = DarkChannel(I,15);
-    A = AtmLight(I,dark);
-    te = TransmissionEstimate(I,A,15);
-    t = TransmissionRefine(src,te);
-    J = Recover(I,t,A,0.1);
-
-    cv2.imshow("dark",dark);
-    cv2.imshow("t",t);
-    cv2.imshow('I',src);
-    cv2.imshow('clahe_then_dark',J);
-    cv2.imwrite("clahe then dark.jpg",J*255);
-    cv2.waitKey();
-    
+    import os
+    # Empty list for reading and appending images.
+    images = []
+    for i in os.listdir('./Fog'):
+        i = "./Fog/"+i
+        if ".jpg" in i or ".png" in i or ".jpeg" in i :
+            img = cv2.imread(i,1)
+            if (img is None):
+                print("Image not read properly",i)
+            images.append(cv2.imread(i, 1))
+            if img is not None: 
+                I = img.astype('float64')/255
+                dark = DarkChannel(I,15)
+                A = AtmLight(I,dark)
+                te = TransmissionEstimate(I,A,15)
+                t = TransmissionRefine(img,te)
+                J = Recover(I,t,A,0.1)
+                for index, img in enumerate(images):
+                    if not os.path.exists("Processed_Images"):
+                        os.mkdir("Processed_Images")
+                        cv2.imwrite("./Processed_Images/output_"+str(index)+".jpg", J*255)
+                    else:
+                        cv2.imwrite("./Processed_Images/output_"+str(index)+".jpg", J*255)
